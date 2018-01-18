24a22f29c53030746465f28e1ccfe8352df7021a
ming <symingel@qq.com>
2018-01-18 20:25:32 +0800

    test

---
+++ Test.php
@@ -0,0 +1,26 @@
+<?php
+phpinfo();
+
+$h=1;
+$ming=date('Y年 m月 d日 H:i:s',strtotime("'+$h hours'"));
+echo $ming;
+
+class Test
+{
+	protected $init;
+	public function __construct($data){
+		$this->init=$data;
+
+	}
+
+	public function show(){
+		$this->init->data();
+
+		echo 'hello world';
+	}
+}
+
+
+// $test=new Test(new Bar);
+// $test->show();
+
